e6857b87a6d3df46ed5a55c7dbac4784ec2c1ffd
Ngwa23 <86411111+Ngwa23@users.noreply.github.com>
2025-01-07 19:05:16 +0100

    updated for deployment

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker  pull ngwa23/simple-python-app:latest
+docker  pull ngwa23/simple-python-app:v1
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 adocker push ngwa23/simple-python-app:latest
+docker run -d -p 5000:5000 gwa23/simple-python-app:v1
